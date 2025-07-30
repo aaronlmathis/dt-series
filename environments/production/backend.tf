54826765d4910e0279da0bc039d8041de51de562
Aaron Mathis <aaron.mathis@gmail.com>
2025-07-30 11:28:42 -0500

    template

--- environments/production/backend.tf
+++ environments/production/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state-prod"
+    resource_group_name  = "rg-terraform-state-production"
     storage_account_name = "tfstateprod1753803789"
     container_name       = "tfstate"
     key                  = "production.terraform.tfstate"
